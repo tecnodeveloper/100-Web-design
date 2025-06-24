e2955b62d916a64f996c11e5eb63ce815d6dbdfb
tecnodeveloper <recluzedev@gmail.com>
2025-06-24 17:20:39 +0500

    Completed the project in future may add the backend part also Admin pannel add to cart products show on admin pannel and do calculation of products and make bill

--- 11-TGM-Frontend-Design/script.js
+++ 11-TGM-Frontend-Design/script.js
@@ -1,18 +1,53 @@
-let leftButton = document.getElementById('left-button');
-let rightButton = document.getElementById('right-button');
-let swipingCard = document.querySelector('.card-ul');
+/* document.querySelectorAll('.left-icon').forEach((button) => {
+    button.addEventListener('click', () => {
+        const cardList = button
+            .closest('.swiping-container')
+            .querySelector('.card-ul');
+        cardList.scrollBy({
+            left: -220,
+            behavior: 'smooth',
+        });
+    });
+});
 
-leftButton.addEventListener('click', leftFunction);
-rightButton.addEventListener('click', rightFunction);
-function leftFunction() {
-    swipingCard.scrollTo({
-        left: swipingCard.scrollLeft - 220,
-        behavior: 'smooth',
+document.querySelectorAll('.right-icon').forEach((button) => {
+    button.addEventListener('click', () => {
+        const cardList = button
+            .closest('.swiping-container')
+            .querySelector('.card-ul');
+        cardList.scrollBy({
+            left: 220,
+            behavior: 'smooth',
+        });
+    });
+});
+ */
+
+let leftButtons = document.getElementsByClassName('left-icon');
+
+for (let i = 0; i < leftButtons.length; i++) {
+    leftButtons[i].addEventListener('click', function () {
+        let swipingContainer = this.closest('.swiping-container');
+
+        let cardList = swipingContainer.querySelector('.card-ul');
+
+        cardList.scrollBy({
+            left: -220,
+            behavior: 'smooth',
+        });
     });
 }
-function rightFunction() {
-    swipingCard.scrollTo({
-        left: swipingCard.scrollLeft + 220,
-        behavior: 'smooth',
+
+let rightButtons = document.getElementsByClassName('right-icon');
+
+for (let j = 0; j < rightButtons.length; j++) {
+    rightButtons[j].addEventListener('click', function () {
+        let swipingContainer = this.closest('.swiping-container');
+        let cardList = swipingContainer.querySelector('.card-ul');
+
+        cardList.scrollBy({
+            left: 220,
+            behavior: 'smooth',
+        });
     });
 }
